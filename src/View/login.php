98bfd3b49041b19847ea153b990410e8a6570b3f
Augustus Winchester <99241183+ZeroFuruya@users.noreply.github.com>
2024-09-30 21:05:51 +0800

    btstrp fix

--- src/View/login.php
+++ src/View/login.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Page</title>
-    <link rel="stylesheet" href="styles/bootstrap-5.3.3-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="styles/bootstrap.min.css">
 </head>
 <body>
     
-    <script src="styles/bootstrap-5.3.3-dist/js/bootstrap.bundle.min.js"></script>
+    <script src="styles/bootstrap.bundle.min.js"></script>
 </body>
 </html>
