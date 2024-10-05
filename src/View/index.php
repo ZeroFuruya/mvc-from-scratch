c7035be4bab4352f1e6b7ea7becddbd4ffa8a330
Augustus Winchester <99241183+ZeroFuruya@users.noreply.github.com>
2024-10-05 12:24:49 +0800

    sd

--- src/View/index.php
+++ src/View/index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Page</title>
     <link rel="stylesheet" href="style/bootstrap/bootstrap.min.css">
-    <link rel="stylesheet" href="style/style.css">
+    <link rel="stylesheet" href="style/mainStyle.css">
   </head>
 <body>
     
