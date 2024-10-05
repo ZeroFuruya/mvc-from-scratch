dc154c68b96fa937e021581899f20f66a0714f19
Augustus Winchester <99241183+ZeroFuruya@users.noreply.github.com>
2024-10-05 12:14:34 +0800

    s

--- src/View/index.php
+++ src/View/index.php
@@ -5,14 +5,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Page</title>
-    <link rel="stylesheet" href="style/bootstrap.min.css">
+    <link rel="stylesheet" href="style/bootstrap/bootstrap.min.css">
+    <link rel="stylesheet" href="style/style.css">
   </head>
 <body>
-    <div class="hello">Hello World</div>
-    <a href="/login">Login</a>
-    <br>
-    <a href="/multiplication">Multiplication</a>
-
+    
+    <div class="grid-container">
+      <div class="title">
+        <a href="/login">Login</a>
+        <a href="/multiplication">Multiplication</a>
+      </div>
+    </div>
 
 </body>
 </html>
--- src/View/login.php
+++ src/View/login.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Page</title>
-    <link rel="stylesheet" href="style/bootstrap.min.css">
+    <link rel="stylesheet" href="style/bootstrap/bootstrap.min.css">
+    <link rel="stylesheet" href="style/style.css">
   </head>
 <body>
 <form>
